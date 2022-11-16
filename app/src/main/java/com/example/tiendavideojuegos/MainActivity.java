07f2d92427485ffba527480573d14a07a1ac2906
natyparamo <natyparamo@gmail.com>
2022-11-15 20:22:02 -0500

    REGISTRO

--- app/src/main/java/com/example/tiendavideojuegos/MainActivity.java
+++ app/src/main/java/com/example/tiendavideojuegos/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.tiendavideojuegos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTextViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewRegister=findViewById(R.id.textViewRegister);
+
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
